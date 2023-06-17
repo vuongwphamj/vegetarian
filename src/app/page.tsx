0526a572db7efca6d575235fec8232847b1d711a
JackPham <vuongphamvnm@gmail.com>
2023-06-17 12:36:24 +0700

    Fixed build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
-'use client'
+"use client";
+import * as React from "react";
 
-import * as React from 'react';
-
-import ProductHero from '@modules/views/AppBody';
-import withRoot from '@modules/withRoot';
-import AppHeader from '@modules/views/AppHeader';
-import AppFooter from '@modules/views/AppFooter';
+import ProductHero from "@modules/views/AppBody";
+import AppHeader from "@modules/views/AppHeader";
+import AppFooter from "@modules/views/AppFooter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "@modules/theme";
 
 function Index() {
   return (
     <React.Fragment>
-      <AppHeader />
-      <ProductHero />
-      <AppFooter />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <React.Fragment>
+          <AppHeader />
+          <ProductHero />
+          <AppFooter />
+        </React.Fragment>
+      </ThemeProvider>
     </React.Fragment>
   );
 }
 
-export default withRoot(Index);
+export default Index;
--- src/modules/withRoot.tsx
+++ src/modules/withRoot.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme';
+import theme from '@modules/theme';
 
 export default function withRoot<P extends JSX.IntrinsicAttributes>(
   Component: React.ComponentType<P>,
@@ -9,7 +9,6 @@ export default function withRoot<P extends JSX.IntrinsicAttributes>(
   function WithRoot(props: P) {
     return (
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...props} />
       </ThemeProvider>
